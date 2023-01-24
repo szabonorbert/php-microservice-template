f9e6b66f9409bfd468866c8b45dea21fb147b154
szabonorbert <szabonorbert@detenyleg.com>
2023-01-24 21:41:57 +0100

    env + readme

--- app/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-
-    $_setting['app_folder'] = "";
-    $_setting['sql_hostname'] = "mysql";
-    $_setting['sql_username'] = "root";
-    $_setting['sql_password'] = "asqw12";
-    $_setting['sql_database'] = "4t";
--- readme.md
+++ readme.md
@@ -1,2 +1,3 @@
 # PHP microservice
-An ideal start of a simple PHP microservice.
+An ideal start of a simple PHP microservice.<br />
+(its not finished yet, come back later)
